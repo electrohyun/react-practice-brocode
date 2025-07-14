406766c8fc1181b4f5ec0a7e159b1d0930bce18f
electrohyun <dev.electrohyun@gmail.com>
2025-07-14 15:23:57 +0900

    7. click events

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function Button() {
+    const handleClick = (e) => {
+        e.target.textContent = "Ouch! 😫";
+    }
+
+    return (
+        <button onDoubleClick={(e) => handleClick(e)}>Click me 😎</button>
+    );
+}
+
+export default Button;
